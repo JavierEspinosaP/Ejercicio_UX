f5987e5143042dd43d8f8aca81fc5c97368a253e
Nacho-sf <serranof.ignacio@gmail.com>
2022-09-30 09:27:08 +0200

    login_structure_html

--- src/components/Main/Login/Login.jsx
+++ src/components/Main/Login/Login.jsx
@@ -2,7 +2,17 @@ import React, { Component } from "react";
 
 class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return <div className={"login-comp"}>
+      <form>
+        <label htmlFor="user">Username</label>
+        <input name="user" id="user" type="text"/>
+
+        <label htmlFor="pass">Password</label>
+        <input name="pass" id="pass" type="password"/>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>;
   }
 }
 
