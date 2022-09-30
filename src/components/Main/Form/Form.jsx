5631f3f9125eccd60b3ad1d12ba0baa51ebf4bf6
Nacho-sf <serranof.ignacio@gmail.com>
2022-09-30 07:42:18 +0200

    form.jsx_structure

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -2,7 +2,17 @@ import React, { Component } from "react";
 
 class Form extends Component {
   render() {
-    return <div>Form</div>;
+    return <div className={"login-comp"}>
+      <form>
+        <label htmlFor="user">Username</label>
+        <input name="user" id="user" type="text"/>
+
+        <label htmlFor="pass">Password</label>
+        <input name="pass" id="pass" type="password"/>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>;
   }
 }
 
